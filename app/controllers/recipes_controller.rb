694eba43d39162453e6f57439dacc311d07b4a02
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-02-16 12:43:53 -0700

    created recipe controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,42 @@
+class RecipeController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+      if recipe.save
+        redirect_to recipes_path
+      else
+        render :new
+      end
+    end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+      if recipe.update(recipe_params)
+        redirect_to recipe_path(@recipe)
+      end
+    end
+
+  def destroy
+    Recipe.destroy(params[:id])
+    redirect_to recipes_path
+  end
+
+  private
+  def recipe_params
+    params.requite(:recipe).permit(:name, :ingredients)
